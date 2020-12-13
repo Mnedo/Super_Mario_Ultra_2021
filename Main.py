08720298609da801cc14773e49646dd4019d6530
mnedo <basecam@yandex.ru>
2020-12-13 22:45:16 +0300

    Появление физики главного персонажа. Основа главного кода.

--- Main.py
+++ Main.py
@@ -1,10 +1,43 @@
 from Mark import Mario
 from Main_platform import Main_platform, Second_platform
-from Objects import  Box, Mob
-
+from Objects import Box, Mob
 
 """
 Код для проверки классов
 
 В дальнейшем рабочий код
 """
+
+import pygame
+
+pygame.init()
+size = width, height = 700, 700
+screen = pygame.display.set_mode(size)
+screen.fill(pygame.Color('black'))
+running = True
+clock = pygame.time.Clock()
+mario = Mario()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                mario.jump()
+                mario.set_jump()
+            if event.key == pygame.K_LEFT:
+                mario.x_move('left')
+            if event.key == pygame.K_RIGHT:
+                mario.x_move('right')
+        if event.type == pygame.KEYUP and event.key != pygame.K_SPACE:
+            mario.x_moving = False
+    screen.fill((0, 0, 0))
+    # rendering()
+    mario.render(pygame, screen)
+    if mario.is_move():
+        pass
+        # render доски возможно
+        clock.tick(18)
+    pygame.display.flip()
--- Mark.py
+++ Mark.py
@@ -1,5 +1,53 @@
-Class Mario:
-    pass
+class Mario:
+    def __init__(self):
+        self.x_pos = 15
+        self.y_pos = 500
+        self.y_stage = 0
+        self.is_jumping = False
+        self.moving = False
+        self.x_moving = False
+        self.vekt = 'pass'
+        self.y_pos_jump = [10, 20, 30, 40, 50, -10, -20, -30, -40, -50]
+
+    def x_move(self, vekt):
+        self.x_moving = True
+        self.vekt = vekt
+        self.movement(vekt)
+
+    def jump(self):
+        self.y_pos -= self.y_pos_jump[self.y_stage]
+        self.y_stage += 1
+        if self.y_stage == len(self.y_pos_jump):
+            self.y_stage = 0
+            self.is_jumping = False
+
+    def movement(self, vekt):
+        if self.is_jumping:
+            self.jump()
+            self.moving = True
+        if vekt == 'left':
+            if self.x_pos - 20 >= 0:
+                self.x_pos -= 20
+                self.moving = True
+        elif vekt == 'right':
+            if self.x_pos + 20 <= 550:
+                self.x_pos += 20
+                self.moving = True
+
+    def render(self, pygame, screen):
+        self.movement('pass')
+        if self.x_moving:
+            self.movement(self.vekt)
+        pygame.draw.circle(screen, (255, 0, 0), (self.x_pos, self.y_pos), 20)
+
+    def is_move(self):
+        if self.moving:
+            self.moving = False
+            return True
+
+    def set_jump(self):
+        self.is_jumping = True
+
 
 """
 Класс главного персонажа - марио
