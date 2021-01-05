7bdc6345c26ef9a7536863377deff0c6df13876c
SuperDasha <dasha_slenik@mail.ru>
2021-01-05 22:03:37 +0300

    Некоторые изменения

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from Mark import Mario
 from Main_platform import SecondPlatform
-# from Objects import MobOnBox, Mob
+from Objects import MobOnBox, Mob
 
 """
 Код для проверки классов
@@ -17,14 +17,21 @@ screen = pygame.display.set_mode(size)
 screen.fill(pygame.Color('black'))
 running = True
 clock = pygame.time.Clock()
+
 all_sprites = pygame.sprite.Group()
-mario_sprites = pygame.sprite.Group()
+# mario_sprites = pygame.sprite.Group()
+
+mob_sprites = pygame.sprite.Group()
+sprite = pygame.sprite.Sprite()
+mob = Mob(mob_sprites)
+
 earth = pygame.sprite.Group()
 entities = pygame.sprite.Group()
 mario = Mario(20, 500, all_sprites)
 platform = SecondPlatform(0, 576)
 entities.add(platform)
 mario.set_walls(entities)
+counter = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -48,13 +55,20 @@ while running:
         if event.type == pygame.KEYUP and event.key != pygame.K_SPACE:
             if mario.moving:
                 mario.set_moving()
-
     screen.fill((0, 0, 0))
     #if mario.have_to_move_earth(width):
     #   render platfrmm
+    mob.move()
+    counter += 1
+    if counter == 2000:
+        counter = 0
+        mob.again()
+    mob_sprites.update()
+    mob_sprites.draw(screen)
+    # mob.touch_jump(350, 350) # должны передаваться координаты марио
+    # mob.fall()  # потом будет проверять, коснулся ли Марио моба
     all_sprites.update()
     all_sprites.draw(screen)
     entities.draw(screen)
-
-    clock.tick(30)
+    clock.tick(330)
     pygame.display.flip()
