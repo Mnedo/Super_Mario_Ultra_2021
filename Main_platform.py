68cdf49c2f42aacf91e063749b175745b82880c1
Нарек <narek.abramian@mail.ru>
2021-01-18 20:52:44 +0300

    И немножко PEP8

--- Main_platform.py
+++ Main_platform.py
@@ -4,15 +4,16 @@ from pygame import *
 
 class MainPlatform(sprite.Sprite):
     def __init__(self, x, y, flag, LENTH):
-        if LENTH == 5000: # Цвет платформы в зависимости от уровня
-            PLATFORM_COLOR = pygame.Color("#8FBC8F")
+        global platform_color, dl
+        if LENTH == 5000:  # Цвет платформы в зависимости от уровня
+            platform_color = pygame.Color("#8FBC8F")
         elif LENTH == 30000:
-            PLATFORM_COLOR = pygame.Color("#87CEEB")
+            platform_color = pygame.Color("#87CEEB")
         elif LENTH == 50000:
-            PLATFORM_COLOR = pygame.Color("#FF6347")
+            platform_color = pygame.Color("#FF6347")
         if not flag:
             sprite.Sprite.__init__(self)
-            if LENTH <= 5000: # Длина платформ в зависимости от уровня
+            if LENTH <= 5000:  # Длина платформ в зависимости от уровня
                 dl = 350
                 # dl = 204
             elif LENTH == 30000:
@@ -20,12 +21,12 @@ class MainPlatform(sprite.Sprite):
             elif LENTH >= 50000:
                 dl = 210
             self.image = Surface((dl, 30))
-            self.image.fill(Color(PLATFORM_COLOR))
+            self.image.fill(Color(platform_color))
             self.rect = Rect(x, y, dl, 20)
         else:
             sprite.Sprite.__init__(self)
             self.image = Surface((LENTH, 50))
-            self.image.fill(Color(PLATFORM_COLOR))
+            self.image.fill(Color(platform_color))
             self.rect = Rect(x, y, LENTH, 30)
 
 
